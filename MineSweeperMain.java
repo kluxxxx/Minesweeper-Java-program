1b2eea4b979c82c74b303f4c6b541612af6f043e
Nathan <nathan69420999@gmail.com>
2026-05-08 11:42:28 -0400

    Class creations

---
+++ MineSweeperMain.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class MineSweeperMain here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MineSweeperMain
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class MineSweeperMain
+     */
+    public MineSweeperMain()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
---
+++ TileState.java
@@ -0,0 +1,11 @@
+
+/**
+ * Enumeration class TileState - write a description of the enum class here
+ *
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum TileState
+{
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
