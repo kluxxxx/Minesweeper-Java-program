ef255a3edd6ca3c822a361ec4259bc8b6a97999d
Darrell <dleun2@ocdsb.ca>
2026-05-08 11:37:22 -0400

    Encapsulator

---
+++ EncapsulatorMain.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class Main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class EncapsulatorMain
+{
+    public static void main(String args[]) {
+        Field.generateDefaultValues("0","UNKNOWN");
+        new EncapsulatorMainFrame();
+    }
+}
