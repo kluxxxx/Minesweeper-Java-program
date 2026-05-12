4cbb867619ef72a52b737bed1a37f9fd684ce512
sharon <sli21@ocdsb.ca>
2026-05-12 10:39:52 -0400

    ok

--- Minefield.java
+++ Minefield.java
@@ -5,7 +5,7 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-   // import arrayList to make the minfield grid
+// import arrayList to make the minfield grid
 import java.util.ArrayList; 
 
 // import javax swing stuff
@@ -13,21 +13,33 @@ import javax.swing.*;
 public class Minefield extends JPanel
 {
     // create arraylist of tiles for the main grid
-    ArrayList<Tile> lstTiles; 
+    private ArrayList<Tile> lstTiles; 
     
     // declare instance variables of type short to store minfield dimensions
-    short shrHeight, shrWidth; 
+    private short shrHeight, shrWidth; 
     
     // declare instance variable of type int for the amount of mines in the minefield
-    int intMines; 
+    private int intMines; 
 
     // constructor for class mineField 
-    public Minefield(short h, short w, int m)
+    public Minefield(short h, short w, int m, ArrayList<Tile> l)
     {
         // initialize instance variables
         this.shrHeight = h; 
         this.shrWidth = w; 
         this.intMines = m; 
+        this.lstTiles = l; 
     }
-
+    
+    // default constructor 
+    public Minefield()
+    {
+        // initialize instance variables
+        this.shrHeight = 0; 
+        this.shrWidth = 0; 
+        this.intMines = 0; 
+        this.lstTiles = new ArrayList<Tile>(); 
+    }
+    
+    
 }
